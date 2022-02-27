db24bcbb9af2a24d4b6d43d516276bae5f23bff4
Abdelkhalek <abdelkhalekamer@outlook.com>
2022-02-27 10:31:15 +0200

    .

--- BackgrountTasks.WebJobs/Program.cs
+++ BackgrountTasks.WebJobs/Program.cs
@@ -4,7 +4,7 @@ namespace BackgrountTasks.WebJobs;
 
 public static class Program
 {
-    private static string CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=stdicom;AccountKey=O0Nc74wHThcMGbLtcBtKLGKeKPmw6+4MeN9yLcwAZf4aQESoLG2nrVKOXZ869MStNFzYAVoS3gXeGo2RA3o4Iw==;EndpointSuffix=core.windows.net";
+    private static string CONNECTION_STRING = "";
 
     private static string CONTAINER_NAME = "dicom-extracted";
 
